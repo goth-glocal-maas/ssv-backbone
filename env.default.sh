0a639e78140c97de932f64ce4b13220f0c8ef302
sipp11 <sipp11@zzyzx.co>
2020-12-15 16:16:14 +0900

    Update env.default

---
+++ env.default.sh
@@ -0,0 +1,13 @@
+
+export ENCRYPTION_KEY='{"type":"RS256", "key": "-----BEGIN PUBLIC KEY-----\nxxxxx\n-----END PUBLIC KEY-----\n"}'
+export AUTH_PRIVATE_KEY=/root/app/private.pem
+export AUTH_PUBLIC_KEY=/root/app/public.pem
+export UPLOAD_DIR=/root/app/media
+export HASURA_ADMIN_SECRET=
+export DATABASE_URL=postgres://
+export DATABASE_PASSWORD=
+export FRONTEND_URL=https://hailing.10z.dev
+export SMTP_SERVER=
+export SMTP_PORT=
+export SMTP_USER=
+export SMTP_PASSWORD=

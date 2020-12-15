16ed8fba4ce956e3856af0b37defde448d50be2a
sipp11 <sipp11@zzyzx.co>
2020-12-15 16:20:12 +0900

    Transport connection

---
+++ supplements/config/transport.js
@@ -0,0 +1,13 @@
+const nodemailer = require("nodemailer")
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_SERVER,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+  secure: true // use TLS
+})
+
+module.exports = transporter
